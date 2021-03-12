4dd62d207a527d770ac380b9e37a122cdac5c62e
Sai Ganesh Kodipelly <ganeshrao9949@gmail.com>
2021-03-12 09:23:58 +0530

    rectified critical bug

--- routes/Auth.js
+++ routes/Auth.js
@@ -92,7 +92,7 @@ router.post('/forgotPass', (req, res, next) => {
                 subject: 'Agile-11 Password Reset',
                 text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
                     'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-                    'http://' + req.headers.host + '/account/reset/' + token + '\n\n' +
+                    'https://' + req.headers.host + '/account/reset/' + token + '\n\n' +
                     'If you did not request this, please ignore this email and your password will remain unchanged.\n'
             };
             smtpTransport.sendMail(mailOptions, err => {
