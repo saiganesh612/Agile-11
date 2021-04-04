53e6d0e7e624e2e59af32e03e5141d979325799d
Sai Ganesh Kodipelly <ganeshrao9949@gmail.com>
2021-04-04 17:14:24 +0530

    refactor the code into controllers

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-const User = require("../models/user");
-const async = require("async");
-const nodemailer = require("nodemailer");
-const crypto = require("crypto");
+const control = require("../controllers/auth");
 
 //Login route
 router.get("/login", (req, res) => {
-    res.render("Auth/login", {style: 'auth'});
+    res.render("Auth/login", { style: 'auth' });
 })
 
 router.post("/login", passport.authenticate('local', {
@@ -20,24 +17,9 @@ router.post("/login", passport.authenticate('local', {
 })
 
 //Signup routes
-router.get("/signup", (req, res) => {
-    res.render("Auth/signup", { style: 'auth' });
-})
-
-router.post("/signup", async (req, res) => {
-    try {
-        const { username, email, password } = req.body;
-        const user = new User({ username, email });
-        const newUser = await User.register(user, password);
-        passport.authenticate('local')(req, res, () => {
-            req.flash("success", `Thanks for registration ${newUser.username}`);
-            res.redirect("/dashboard");
-        })
-    } catch (e) {
-        req.flash("error", e.message);
-        res.redirect("/account/signup");
-    }
-})
+router.route("/signup")
+    .get(control.signupForm)
+    .post(control.signup)
 
 //Logout routes
 router.get("/logout", (req, res) => {
@@ -46,132 +28,16 @@ router.get("/logout", (req, res) => {
     res.redirect("/home");
 })
 
-
 // ===========================Forgot Password Settings=====================================
-
-
 // Forgot password routes
-router.get("/forgotPass", (req, res) => {
-    res.render("Auth/forgot", { style: 'auth' });
-})
-
-router.post('/forgotPass', (req, res, next) => {
-    async.waterfall([
-        done => {
-            crypto.randomBytes(20, (err, buf) => {
-                const token = buf.toString('hex');
-                done(err, token);
-            });
-        },
-        (token, done) => {
-            User.findOne({ email: req.body.email }, (err, user) => {
-                if (!user) {
-                    req.flash('error', 'No account with this email address was registered');
-                    return res.redirect('/account/forgotPass');
-                }
-
-                user.resetPasswordToken = token;
-                user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
-
-                user.save(err => {
-                    done(err, token, user);
-                })
-            });
-        },
-        (token, user, done) => {
-            const smtpTransport = nodemailer.createTransport({
-                service: 'Gmail',
-                auth: {
-                    user: process.env.GMAIL_ADDRESS,
-                    pass: process.env.GMAIL_PASS
-                }
-            });
-            const mailOptions = {
-                to: user.email,
-                from: process.env.GMAIL_ADDRESS,
-                subject: 'Agile-11 Password Reset',
-                text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
-                    'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-                    'https://' + req.headers.host + '/account/reset/' + token + '\n\n' +
-                    'If you did not request this, please ignore this email and your password will remain unchanged.\n'
-            };
-            smtpTransport.sendMail(mailOptions, err => {
-                console.log('mail sent');
-                req.flash('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
-                done(err, 'done');
-            });
-        }
-    ], err => {
-        if (err) return next(err);
-            res.redirect('/account/forgotPass');
-    });
-});
-
+router.route("/forgotPass")
+    .get(control.forgotPasswordForm)
+    .post(control.forgotPassword);
 
 // ========================Reset Password Settings==========================
-
-
 // Reset routes
-router.get("/reset/:token", (req, res) => {
-    const { token } = req.params;
-    User.findOne({ resetPasswordToken: token, resetPasswordExpires: { $gt: Date.now() } }, (err, user) => {
-        if (!user) {
-            req.flash('error', 'Password reset token is invalid or has expired.');
-            return res.redirect('/account/forgotPass');
-        }
-        res.render('Auth/reset', { token: token, style: 'auth' });
-    });
-})
-
-router.post('/reset/:token', (req, res) => {
-    const { token } = req.params;
-    async.waterfall([
-        done => {
-            User.findOne({ resetPasswordToken: token, resetPasswordExpires: { $gt: Date.now() } }, (err, user) => {
-                if (!user) {
-                    req.flash('error', 'Password reset token is invalid or has expired.');
-                    return res.redirect('back');
-                }
-                if (req.body.password === req.body.confirm) {
-                    user.setPassword(req.body.password, err => {
-                        user.resetPasswordToken = undefined;
-                        user.resetPasswordExpires = undefined;
-
-                        user.save( err => {
-                            req.logIn(user, err => {
-                                done(err, user);
-                            });
-                        });
-                    })
-                } else {
-                    req.flash("error", "Passwords do not match.");
-                    return res.redirect('back');
-                }
-            });
-        },
-        (user, done) => {
-            const smtpTransport = nodemailer.createTransport({
-                service: 'Gmail',
-                auth: {
-                    user: process.env.GMAIL_ADDRESS,
-                    pass: process.env.GMAIL_PASS
-                }
-            });
-            const mailOptions = {
-                to: user.email,
-                from: process.env.GMAIL_ADDRESS,
-                subject: 'Your password has been changed',
-                text: 'Hello,\n\n' +
-                    'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
-            };
-            smtpTransport.sendMail(mailOptions, err => {
-                req.flash('success', 'Success! Your password has been changed.');
-                done(err);
-            });
-        }
-    ], err => {
-        res.redirect('/dashboard');
-    });
-});
+router.route("/reset/:token")
+    .get(control.resetPasswordForm)
+    .post(control.resetPassword);
 
 module.exports = router;
--- routes/userInfo.js
+++ routes/userInfo.js
@@ -1,248 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
-const Room = require("../models/room");
 const { isLoggedIn } = require("../middleware/index");
-const formatMsg = require("../utils/message");
-const { joinUser, getCurrentUser, userLeft, getRoomUsers, getDetails } = require("../utils/users");
-const iplSeries = require("../public/seriesdata/ipl");
-const ipl = iplSeries();
+const room = require("../controllers/rooms");
+const enter = require("../controllers/roomentry");
+const account = require("../controllers/account");
 
 // Display's player stats
-router.get("/dashboard", isLoggedIn, async (req, res) => {
-    const { _id, username } = req.user;
-    const listOfRooms = await Room.find({ $or: [{ players: { $in: [_id] } }, { "admin.username": { $eq: username } }] });
-    res.render("userInfo/dashboard", { style: 'dashboard', rooms: listOfRooms.reverse() });
-})
+router.get("/dashboard", isLoggedIn, enter.dashboard)
 
 // Creates a new room and save it to database
-router.post("/createroom", isLoggedIn, async (req, res) => {
-    try {
-        const { roomname } = req.body;
-        const { _id, username } = req.user;
-        let admin = { id: _id, username };
-        let ba = { balanceAmount: 100, name: username }
-        const user = await User.findOne({ username: { $eq: username } })
-        if (!user) {
-            req.flash("error", "U don't have permission to create room!!");
-            res.redirect("/account/login");
-        } else {
-            const newRoom = new Room({ roomName: roomname, admin });
-            newRoom.teams.push(ba)
-            await newRoom.save();
-            user.rooms.unshift(newRoom);
-            await user.save();
-            req.flash("success", "Share your room details with your friends and start playing.")
-            res.redirect(`/room/${newRoom._id}`);
-        }
-    } catch (e) {
-        req.flash("error", "Oh no! Something went wrong.");
-        res.redirect("/dashboard");
-    }
-})
+router.post("/createroom", isLoggedIn, enter.createRoom)
 
 // Search for the room and enters the room
-router.post("/enterroom", isLoggedIn, async (req, res) => {
-    try {
-        const { roomname, roomid } = req.body;
-        const { username, _id } = req.user;
-        const user = await User.findOne({ username: { $eq: username } });
-        if (!user) throw "You don't have permission to play in rooms.";
-        const room = await Room.findOne({ _id: { $eq: roomid }, roomName: { $eq: roomname } });
-
-        //Check whether room was created or not
-        if (!room) throw "Room was not found!";
-
-        // Checks ownership of the room with current user
-        if (room.admin.username === username) throw "You created this room entering room will duplicates player in the room."
-
-        // Check whether the room is filled or not
-        if (room.players.length >= 7) throw "The room is filled!"
-
-        // Check whether current user was already in room or not
-        const dp = room.players.find(pid => pid.equals(_id));
-        if (dp) throw "You already there in this room"
-
-        // If there is no exception this code will be executed
-        let ba = { balanceAmount: 100, name: username }
-        user.rooms.unshift(room);
-        room.players.unshift(user);
-        room.teams.push(ba);
-        await user.save();
-        await room.save();
-        res.redirect(`/room/${room._id}`);
-    } catch (e) {
-        req.flash("error", e);
-        res.redirect("/dashboard");
-    }
-})
+router.post("/enterroom", isLoggedIn, enter.enterRoom)
 
 // Core logic for creating the rooms and opens a socket
-router.get("/room/:roomid", isLoggedIn, (req, res) => {
-    const io = global.socketIO;
-    io.once("connection", socket => {
-        // Join in particular room
-        let userid, rd;
-        socket.on("joinRoom", async ({ user, room }) => {
-            // Retrive room and user details from database
-            const userDetails = await User.findOne({ username: { $eq: user } })
-            const roomDetails = await Room.findById(room);
-            const { roomName, _id } = roomDetails;
-            userid = userDetails._id;
-            rd = roomDetails;
+router.get("/room/:roomid", isLoggedIn, room.startPlaying)
 
-            const puser = joinUser(userid, user, roomName);
-
-            socket.join(puser.room);
-
-            // Welcome current user
-            socket.emit("message", formatMsg("Agile-11", `Welcome to ${puser.room} room`));
-
-            //Broadcast when user joins
-            socket.broadcast
-                .to(puser.room)
-                .emit("message", formatMsg("Agile-11", `${puser.username} joined in the room`));
-
-            // Send users and room info
-            io.to(puser.room).emit("roomUsers", {
-                id: _id,
-                room: puser.room,
-                users: getRoomUsers(puser.room)
-            })
-
-            // Updates user balance amount
-            const index = rd.teams.findIndex(p => p.name === puser.username);
-            socket.emit("balance", { balance: rd.teams[index].balanceAmount });
-
-            // Send the teams list to the user
-            socket.emit("lop", { currentTeamData: rd.teams })
-        })
-
-        // Listen for chat message
-        socket.on("chatMessage", mssg => {
-            let user = getCurrentUser(userid);
-
-            io.to(user.room).emit("message", formatMsg(user.username, mssg))
-        })
-
-        // Core logic for team selection
-        socket.on("bet", async ({ money, name }) => {
-            let user = getCurrentUser(userid);
-            let check;
-
-            // Checks whether the given player name was there in the given list 
-            for (let p in ipl) {
-                check = ipl[p].filter(obj => obj.name === name)
-                if (check.length > 0) break;
-            }
-
-            if (check.length === 0) {
-                io.to(user.room)
-                    .emit("message", formatMsg("Agile-11", `${user.username} your selected player was not there in the list. We recommend you to check the player name in the list or copy and paste that player`))
-            } else {
-
-                // Get details of current user  
-                let info = getDetails(userid, money, name, rd);
-
-                if (typeof info === 'string') {
-                    socket.emit("message", formatMsg("Agile-11", info))
-                } else {
-
-                    const { details, lou } = info;
-                    // Checks whether any one left with the betting process
-                    if (details.length !== lou.length) {
-
-                        const remainingPlayers = lou.filter(user => {
-                            return !details.some(u => {
-                                return user.username === u.username
-                            })
-                        })
-                        socket.emit("bul", { users: remainingPlayers })
-
-                    } else {
-                        // Get the winner in the room
-                        const winner = details.reduce((prev, current) => {
-                            return (prev.money === current.money) ? prev : (prev.money > current.money) ? prev : current
-                        })
-                        if (winner.money !== 0) {
-                            // Save these winner data in database and assign player to the winner
-                            const index = rd.teams.findIndex(p => p.name === winner.username);
-                            let cb = rd.teams[index].balanceAmount;
-
-                            // Round the balance to 1 decimal value
-                            rd.teams[index].balanceAmount = Math.round((cb - winner.money) * 10) / 10;
-                            rd.teams[index].players.push(name);
-                            await rd.save();
-
-                            // Updates user balance amount
-                            if (user.username === winner.username) {
-                                socket.emit("balance", { balance: rd.teams[index].balanceAmount });
-                            } else {
-                                const index = rd.teams.findIndex(p => p.name === user.username);
-                                socket.emit("balance", { balance: rd.teams[index].balanceAmount });
-                            }
-
-                            // Updates the teams list for every user in the room
-                            io.to(user.room)
-                                .emit("lop", { currentTeamData: rd.teams })
-
-                            io.to(user.room)
-                                .emit("message", formatMsg("Agile-11", `${winner.username} has won ${name} and kept ${winner.money}Cr/-`))
-                        } else {
-                            io.to(user.room)
-                                .emit("message", formatMsg("Agile-11", `None of your team mates selected ${name} and kept unsold.`))
-                        }
-                    }
-                }
-            }
-        })
-
-        //Runs when clients disconnect
-        socket.on("disconnect", () => {
-            const user = userLeft(userid);
-
-            if (user) {
-                io.to(user.room)
-                    .emit("message", formatMsg("Agile-11", `${user.username} left the room`));
-
-                // Send users and room info
-                io.to(user.room).emit("roomUsers", {
-                    id: user.id,
-                    room: user.room,
-                    users: getRoomUsers(user.room)
-                })
-            }
-        })
-    });
-    res.render("rooms/room", { style: "room", id: req.params.roomid, ipl });
-})
-
-router.get("/settings", isLoggedIn, (req, res) => {
-    res.render("userInfo/settings", { style: 'settings' });
-})
-
-router.put("/settings", isLoggedIn, async (req, res) => {
-    try {
-        const { username, email } = req.body;
-        const updatedUser = await User.findByIdAndUpdate(req.user._id, { username, email }, { new: true });
-        req.flash("success", "Changes saved successfully");
-        res.redirect("/settings");
-    } catch (e) {
-        req.flash("error", "Oho no, something went wrong!!");
-        res.redirect("/settings");
-    }
-})
-
-router.delete("/destroy", isLoggedIn, async (req, res) => {
-    try {
-        const id = req.user._id;
-        await User.findByIdAndDelete(id);
-        req.flash("success", "Awww, we would like to see you back!!");
-        res.redirect("home");
-    } catch (e) {
-        req.flash("error", e.message);
-        res.redirect("back");
-    }
-})
+// Account info
+router.route("/settings")
+    .get(isLoggedIn, account.renderForm)
+    .put(isLoggedIn, account.updateDetails)
+    .delete(isLoggedIn, account.deleteAccount)
 
 module.exports = router;
