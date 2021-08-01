b975e443bf5bf133dbd1a4d7ac51cb22f78b8f1c
Sai Ganesh Kodipelly <ganeshrao9949@gmail.com>
2021-08-01 14:18:42 +0530

    resolve updation vulnerability

--- controllers/account.js
+++ controllers/account.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const Room = require("../models/room")
 
 module.exports.renderForm = (req, res) => {
     res.render("userInfo/settings", { style: 'settings' });
@@ -7,7 +8,15 @@ module.exports.renderForm = (req, res) => {
 module.exports.updateDetails = async (req, res) => {
     try {
         const { username, email } = req.body;
-        const updatedUser = await User.findByIdAndUpdate({ $eq: req.user._id }, { username, email }, { new: true });
+        const updatedUser = await User.findOneAndUpdate({ _id: { $eq: req.user._id } }, { username, email }, { new: true });
+        if (updatedUser.username !== req.user.username) {
+            console.log("in if")
+            const rooms = await Room.find({ "admin.id": { $eq: req.user._id } })
+            rooms.map(async room => {
+                room.admin.username = updatedUser.username
+                await room.save()
+            })
+        }
         req.flash("success", "Changes saved successfully");
         res.redirect("/settings");
     } catch (e) {
@@ -19,7 +28,7 @@ module.exports.updateDetails = async (req, res) => {
 module.exports.deleteAccount = async (req, res) => {
     try {
         const id = req.user._id;
-        await User.findByIdAndDelete(id);
+        await User.findOneAndRemove({ _id: { $eq: id } });
         req.flash("success", "Awww, we would like to see you back!!");
         res.redirect("home");
     } catch (e) {
